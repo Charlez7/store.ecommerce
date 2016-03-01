f2af53605c5030e906df4ff0ac1da7e09d460957
Charlez7 <charles.dias@outlook.com>
2016-03-01 08:48:37 -0300

    Initial Commit

---
+++ store.ecommerce.utest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace store.ecommerce.utest
+{
+    [TestClass]
+    public class UnitTestStore
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
